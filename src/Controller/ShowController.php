01236a108b945b83d0553c9e1ef1c8201c0db181
Margot BIGNON <margot.bignon78@gmail.com>
2024-04-12 12:17:32 +0200

    add ShowController

---
+++ src/Controller/ShowController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ShowController extends AbstractController
+{
+    #[Route('/{parent_slug}/{slug}', name: 'app_maincategory_category')]
+    #[Route('/{slug}', name: 'app_maincategory')]
+    public function index(Category $category, CategoryRepository $categoryRepository): Response
+    {
+        
+        $shows = [];
+        if (!$category->getParent()) {
+            $childCategories= $categoryRepository->findBy(['parent' => $category]);
+
+            foreach ($childCategories as $childCategory) {
+                $categoryShows = $childCategory->getShows()->toArray();
+                $shows = array_merge($shows, $categoryShows);
+
+            }
+        } else {
+            $shows = $category->getShows();
+        }
+        if (!$category) {
+            throw $this->createNotFoundException('La catégorie demandée n\'existe pas.');
+        }
+
+        return $this->render('show/index.html.twig', [
+            'shows' => $shows,
+        ]);
+    }
+}
